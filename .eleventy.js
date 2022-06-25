e7c2f87b93c45d5784f7b9fcd3565af93745c7fe
Thomas Rutzer <hi@thomasrutzer.dev>
2022-06-25 16:52:08 +0200

    Improve critical css management with clean-css

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
+const CleanCSS = require("clean-css");
+const fs = require("fs");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+
+  eleventyConfig.addFilter("cssmin", (pageId) => {
+    const css = fs.readFileSync(`src/css/${pageId}.critical.css`, "utf8");
+    return new CleanCSS({}).minify(css).styles;
+  });
+
   return {
     passthroughFileCopy: true,
   };
