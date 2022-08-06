e55cb3c47b3fe7a873d23d6c0f2d8a9f5b445eaa
Thomas Rutzer <hi@thomasrutzer.dev>
2022-08-06 17:00:24 +0200

    Add more page animations to index paage

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,41 @@
-import { animate, scroll, stagger, timeline } from "motion";
+import {
+  animate,
+  scroll,
+  ScrollOffset,
+  stagger,
+  timeline,
+  inView,
+} from "motion";
 import Splitting from "splitting";
 
-const EaseLinear = "cubic-bezier(0.25,0.25,0.75,0.75)";
-const EaseInSine = "cubic-bezier(0.47,0,0.745,0.715)";
-const EaseoutSine = "cubic-bezier(0.39,0.575,0.565,1)";
-const EaseinOutSine = "cubic-bezier(0.445,0.05,0.55,0.95)";
-const EaseinQuad = "cubic-bezier(0.55,0.085,0.68,0.53)";
-const EaseoutQuad = "cubic-bezier(0.25,0.46,0.45,0.94)";
-const EaseinOutQuad = "cubic-bezier(0.455,0.03,0.515,0.955)";
-const EaseinCubic = "cubic-bezier(0.55,0.055,0.675,0.19)";
-const EaseoutCubic = "cubic-bezier(0.215,0.61,0.355,1)";
-const EaseinOutCubic = "cubic-bezier(0.645,0.045,0.355,1)";
-const EaseinQuart = "cubic-bezier(0.895,0.03,0.685,0.22)";
-const EaseoutQuart = "cubic-bezier(0.165,0.84,0.44,1)";
-const EaseinOutQuart = "cubic-bezier(0.77,0,0.175,1)";
-const EaseinQuint = "cubic-bezier(0.755,0.05,0.855,0.06)";
-const EaseoutQuint = "cubic-bezier(0.23,1,0.32,1)";
-const EaseinOutQuint = "cubic-bezier(0.86,0,0.07,1)";
-const EaseinExpo = "cubic-bezier(0.95,0.05,0.795,0.035)";
-const EaseoutExpo = "cubic-bezier(0.19,1,0.22,1)";
-const EaseinOutExpo = "cubic-bezier(1,0,0,1)";
-const EaseinCirc = "cubic-bezier(0.6,0.04,0.98,0.335)";
-const EaseoutCirc = "cubic-bezier(0.075,0.82,0.165,1)";
-const EaseinOutCirc = "cubic-bezier(0.785,0.135,0.15,0.86)";
-const EaseinBack = "cubic-bezier(0.6,-0.28,0.735,0.045)";
-const EaseoutBack = "cubic-bezier(0.25,2,0.5,0.9)";
-const EaseinOutBack = "cubic-bezier(0.68,-0.55,0.265,1.55)";
+function getCSSCustomProp(propKey, element = document.body, castAs = "string") {
+  let response = getComputedStyle(element).getPropertyValue(propKey);
+
+  if (response.length) {
+    response = response.replace(/\'|"/g, "").trim();
+  }
+
+  switch (castAs) {
+    case "number":
+    case "int":
+      return parseInt(response, 10);
+    case "float":
+      return parseFloat(response, 10);
+    case "boolean":
+    case "bool":
+      return response === "true" || response === "1";
+  }
+
+  return response;
+}
+
+const EaseoutSine = getCSSCustomProp("--ease-out-sine");
+const EaseinQuad = getCSSCustomProp("--ease-in-quad");
+const EaseoutCubic = getCSSCustomProp("--ease-out-cubic");
+
+function setCssCustomProp(propKey, element, value) {
+  element.style.setProperty(propKey, value);
+}
 
 function shuffle(arr) {
   let currentIndex = arr.length,
@@ -44,22 +54,7 @@ function shuffle(arr) {
   return arr;
 }
 
-function getRandomSub(arr, size = 3) {
-  const shuffled = arr.slice(0);
-  let i = arr.length,
-    temp,
-    index;
-
-  while (i--) {
-    index = Math.floor((i + 1) * Math.random());
-    temp = shuffled[index];
-    shuffled[index] = shuffled[i];
-    shuffled[i] = temp;
-  }
-  return shuffled.slice(0, size);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
+function initPageAnimations() {
   const background = document.querySelector("[data-page-animation-background]");
   const container = document.querySelector("[data-page-animation-container]");
   const content = document.querySelector("[data-page-animation-content]");
@@ -83,37 +78,12 @@ document.addEventListener("DOMContentLoaded", () => {
   if (content) {
     sequence.push([
       content,
-      { opacity: [0, 1], y: [16, 0] },
+      { opacity: [0, 1] },
       { easing: EaseoutCubic, at: "-0.75" },
     ]);
   }
 
-  timeline(sequence).finished.then(() => {
-    let initialOffsetAnimation = false;
-    const randomChars = getRandomSub(chars, Math.round(chars.length / 3));
-
-    scroll(
-      ({ y }) => {
-        if (!initialOffsetAnimation) {
-          animate(randomChars, {
-            y: [0, `${y.progress * 100}%`],
-            easing: EaseinOutBack,
-          });
-
-          initialOffsetAnimation = true;
-        }
-
-        animate(randomChars, {
-          y: `${y.progress * 100}%`,
-        });
-      },
-
-      {
-        target: container,
-        offset: ["end end", "end start"],
-      }
-    );
-  });
+  timeline(sequence);
 
   if (background) {
     scroll(animate(background, { opacity: [0, 1, 1, 0] }), {
@@ -121,4 +91,88 @@ document.addEventListener("DOMContentLoaded", () => {
       offset: ["start start", "end end", "start start", "end start"],
     });
   }
+}
+
+function intiScrollProgressAnimations() {
+  const scrollProgressContainer = document.querySelectorAll(
+    "[data-page-animation-scroll-progress]"
+  );
+
+  Array.from(scrollProgressContainer).map((container) => {
+    scroll(
+      ({ y }) => {
+        setCssCustomProp("--scroll-progress", container, y.progress);
+      },
+      {
+        target: container,
+        offset: ["center end", "end end"],
+      }
+    );
+  });
+}
+
+function initFadeInViewAnimations() {
+  const allAnimations = Array.from(
+    document.querySelectorAll("[data-page-animation-fade-in-view]")
+  );
+
+  Array.from(allAnimations).map((container) => {
+    container.style.opacity = 0;
+
+    inView(
+      container,
+      (info) => {
+        animate(info.target, { opacity: [0, 1] }, { duration: 1 });
+      },
+      { amount: 0.5 }
+    );
+  });
+}
+
+function initCustomClassAnimations() {
+  const allAnimations = document.querySelectorAll(
+    "[data-page-animation-custom-class-in-view]"
+  );
+
+  Array.from(allAnimations).map((container) => {
+    const baseCls = container.getAttribute(
+      "data-page-animation-custom-class-in-view"
+    );
+
+    container.classList.add(`${baseCls}--not-in-view`);
+
+    inView(
+      container,
+      (info) => {
+        info.target.classList.remove(`${baseCls}--not-in-view`);
+        info.target.classList.add(`${baseCls}--in-view`);
+      },
+      { amount: 0.5 }
+    );
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initPageAnimations();
+  intiScrollProgressAnimations();
+  initFadeInViewAnimations();
+  initCustomClassAnimations();
+
+  inView(
+    document.querySelector(".short-bio__image"),
+    (info) => {
+      timeline([
+        [info.target, { opacity: [0, 1] }, { duration: 0.5 }],
+        [
+          info.target.querySelector("img"),
+          {
+            scale: [1.2, 1],
+            filter: ["brightness(2)", "brightness(1)"],
+          },
+          { easing: EaseoutSine, duration: 0.7, at: "-0.5" },
+        ],
+      ]);
+    },
+    { amount: 0.5 }
+  );
 });
