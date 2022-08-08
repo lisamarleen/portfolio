433ef18fbfa1eb5a9135e376806750a858dfb396
Thomas Rutzer <hi@thomasrutzer.dev>
2022-08-08 21:07:55 +0200

    Add skipLink to home

--- src/js/home.critical.js
+++ src/js/home.critical.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.querySelector(".intro__video");
+  const skipLink = document.querySelector("[href='#about']");
+
+  skipLink.addEventListener("click", () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
 
   video.classList.add("intro__video--with-fade");
   video.addEventListener("canplay", () =>
     video.classList.add("intro__video--loaded")
   );
+
+
 });
